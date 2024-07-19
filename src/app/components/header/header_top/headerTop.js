7e13857f3bc1243f7f5a100b910d72bc8ac27c04
ADRIAN_MIESZCZAK <mieszczak22@gmail.com>
2024-07-19 09:13:37 +0200

    - clearing  old code; - camelCase naming of classes in 'headerTop' component; - adding proper expressions of classes in 'headerTop';

--- src/app/components/header/header_top/headerTop.js
+++ src/app/components/header/header_top/headerTop.js
@@ -1,32 +1,30 @@
-import "./header_top.css";
-/* import logo from "/01_header/papużka_logo_linear closed.svg"; */
-/* import user from "./01_header/user.svg"; */
-/* const titlehead = require("./01_header/Frame 15.png"); */
+import styles from "./headerTop.module.css";
 import Image from "next/image";
+import classNames from "classnames";
 
 
 export default function HeaderTop() {
   
   return (
-    <div className="header-top">
-      <Image className="header-icon header-logo" 
+    <div className={styles.headerTop}>
+      <Image className={classNames(styles.headerIcon, styles.headerLogo)}
       src='/papużka_logo_linear_closed.svg'
       width={200}
       height={200}
-      alt="logo" />
+      alt="logo" /> 
       <Image
-        className="header-icon header-icon-middle"
+        className={classNames(styles.headerIcon, styles.headerIconMiddle)}
         width={639}
         height={220}
         src='/Frame15.png'
         alt="logo text"
-      />
-      <Image className="header-icon header-user" 
+      /> 
+      <Image className={classNames(styles.headerIcon, styles.headerUser)}
       src='/user.svg'
       width={200}
       height={200}
       alt='user-icon' 
-      />
+      /> 
     </div>
     
   );

41504449e13436a6d098fac2e43171b2f9d776a7
ADRIAN_MIESZCZAK <mieszczak22@gmail.com>
2024-07-27 14:40:32 +0200

    - applying id's for headerTop elements; - applying key to rendered elements of list;

--- src/app/components/header/header_top/headerTop.js
+++ src/app/components/header/header_top/headerTop.js
@@ -1,31 +1,25 @@
+"use client";
 import styles from "./headerTop.module.css";
 import Image from "next/image";
 import classNames from "classnames";
-
+import { headerTopData } from "@/app/data/headerTopData/headerTopData.js";
 
 export default function HeaderTop() {
-  
   return (
     <div className={styles.headerTop}>
-      <Image className={classNames(styles.headerIcon, styles.headerLogo)}
-      src='/papużka_logo_linear_closed.svg'
-      width={200}
-      height={200}
-      alt="logo" /> 
-      <Image
-        className={classNames(styles.headerIcon, styles.headerIconMiddle)}
-        width={639}
-        height={220}
-        src='/Frame15.png'
-        alt="logo text"
-      /> 
-      <Image className={classNames(styles.headerIcon, styles.headerUser)}
-      src='/user.svg'
-      width={200}
-      height={200}
-      alt='user-icon' 
-      /> 
+      {headerTopData.map((item) => {
+        return (
+          <Image
+            className={classNames(
+              item.classNames.map((myClass) => styles[myClass])
+            )}
+            src={item.src}
+            width={item.width}
+            height={item.height}
+            alt={item.alt}
+          />
+        );
+      })}
     </div>
-    
   );
 }
--- src/app/data/headerTopData/headerTopData.js
+++ src/app/data/headerTopData/headerTopData.js
@@ -1,14 +1,11 @@
 const headerTopData = [
-    {class: 'styles.headerLogo', src:'/papużka_logo_linear_closed.svg', width: 200, height: 200, alt: 'logo'},
-    {class: 'styles.headerIconMiddle', src:'/Frame15.png', width: 639, height: 220, alt: 'logo text'},
-    {class: 'styles.headerUser', src:'/user.svg', width: 200, height: 200, alt: 'user-icon'},
+        {classNames: ['headerIcon', 'headerLogo'], src:'/papużka_logo_linear_closed.svg', width: 200, height: 200, alt: 'logo'},
+        {classNames: ['headerIcon', 'headerIconMiddle'], src:'/Frame15.png', width: 639, height: 220, alt: 'logo text'},
+        {classNames: ['headerIcon', 'headerUser'], src:'/user.svg', width: 200, height: 200, alt: 'user-icon'},       
 ]
 
 export { headerTopData };
 
 
-/* const headerTopData = [
-    {classNames: ['headerIcon', 'headerLogo'], src:'/papużka_logo_linear_closed.svg', width: 200, height: 200, alt: 'logo'},
-    {classNames: ['headerIcon', 'headerIconMiddle'], src:'/Frame15.png', width: 639, height: 220, alt: 'logo text'},
-    {classNames: ['headerIcon', 'headerUser'], src:'/user.svg', width: 200, height: 200, alt: 'user-icon'},
-] */
+
+

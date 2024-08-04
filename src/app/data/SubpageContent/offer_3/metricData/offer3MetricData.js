965fa32227a3e408403a67afc6550d122d29dd01
ADRIAN_MIESZCZAK <mieszczak22@gmail.com>
2024-08-04 13:04:14 +0200

    - informations for 'metric info' component in 'offer3' subpage; - organization of elements on 'offer3' page; - list template module in 'offer3' - clearing old code;

--- src/app/data/SubpageContent/offer_3/metricData/offer3MetricData.js
+++ src/app/data/SubpageContent/offer_3/metricData/offer3MetricData.js
@@ -1,9 +1,9 @@
 const offer3MetricData = [
     {src: '/offer3/u_calendar-alt.svg', alt: 'ikona kalendarz', title:'Plan', description: '12 cotygodniowych spotkań około 60 minut raz w tygodniu'},
-    {src: '/offer3/u_home-alt.svg', alt: 'ikona dom', title:'Kontakt', description: 'W okresie przeprowadzania terapii jestemy w kontakcie z rodzicem'},
-    {src: '/offer3/u_phone-alt.svg', alt: 'ikona telefon', title:'Nauka przez zabawę', description: 'Nasze zajęcia w odróżnieniu od wielu tego typu programów uczę przy jednocześnie mile spędzonym przez dziecko czasie'},
-    {src: '/offer3/u_chat-info.svg', alt: 'ikona wiadomość', title:'Szybka Interwencja', description: 'w przypadku problemów wymagających szybkiej interwencji służymy pomocą'}, 
-    {src: '/offer3/award-alt.svg', alt: 'ikona odznaczenie', title: 'Wspólne wizyty', description: 'Jeśli zachodzi potrzeba uczestnictwa rodzica w zajęciach, zaprasamy do wspólnej wizyty'}
+    {src: '/offer3/u_home-alt.svg', alt: 'ikona dom', title:'Działania własne', description: 'plan zakłada ćwiczenia utrwalające do wykonania w domu'},
+    {src: '/offer3/u_phone-alt.svg', alt: 'ikona telefon', title:'Kontakt', description: 'kontakt z trenerem i wsparcie w trakcie trwania treningu'},
+    {src: '/offer3/u_chat-info.svg', alt: 'ikona wiadomość', title:'Konsultacje', description: 'konsultacja z rodzicem po zakończeniu treningu- do 30 minut'}, 
+    {src: '/offer3/award-alt.svg', alt: 'ikona odznaczenie', title: 'Podsumowanie', description: 'dyplom ukończenia treningu i opis funkcjonowania dziecka'}
 ]
 
 export { offer3MetricData };
--- src/app/offer_3/page.js
+++ src/app/offer_3/page.js
@@ -7,6 +7,9 @@ import { listTitleData } from "../data/SubpageContent/offer_3/listTitleData/offe
 //
 import MetricUnit from "../components/metricUnit/metricUnit.js";
 import { offer3MetricData } from "../data/SubpageContent/offer_3/metricData/offer3MetricData";
+//
+import ListTemplate from "../componentSets/PageTemplate/ListTemplate/ListTemplate.js";
+import { offer3ListData } from "../data/SubpageContent/offer_3/listData/offer3ListData.js";
 
 export default function Offer_3() {
   return (
@@ -27,125 +30,32 @@ export default function Offer_3() {
           </section>
 
           <section className={styles.halfDivider}>
-            {offer3MetricData.map((item) => {
-              return (
-                <MetricUnit
-                  src={item.src}
-                  alt={item.alt}
-                  title={item.title}
-                  description={item.description}
-                />
-              );
-            })}
-
-            {/* <aside>
-              <div className="metric">
-                <div className="metric-icon">
-                  <img src="img/aside/offer_3/u_calendar-alt.svg" />
-                </div>
-
-                <div className="metric-info">
-                  <h2>PLAN</h2>
-
-                  <p>12 cotygodniowych spotkań około 60 minut raz w tygodniu</p>
-                </div>
-              </div>
-
-              <div className="metric">
-                <div className="metric-icon">
-                  <img
-                    className="home"
-                    src="img/aside/offer_3/u_home-alt.svg"
+            <aside>
+              {offer3MetricData.map((item) => {
+                return (
+                  <MetricUnit
+                    src={item.src}
+                    alt={item.alt}
+                    title={item.title}
+                    description={item.description}
                   />
-                </div>
-
-                <div className="metric-info">
-                  <h2>DZIAŁANIA WŁASNE</h2>
-
-                  <p>plan zakłada ćwiczenia utrwalające do wykonania w domu</p>
-                </div>
-              </div>
-
-              <div className="metric">
-                <div className="metric-icon">
-                  <img
-                    className="phone"
-                    src="img/aside/offer_3/u_phone-alt.svg"
-                  />
-                </div>
-
-                <div className="metric-info">
-                  <h2>KONTAKT</h2>
-
-                  <p>
-                    kontakt z trenerem i wsparcie w trakcie trwania treningu
-                  </p>
-                </div>
-              </div>
-
-              <div className="metric">
-                <div className="metric-icon">
-                  <img
-                    className="chat"
-                    src="img/aside/offer_3/u_chat-info.svg"
-                  />
-                </div>
-
-                <div className="metric-info">
-                  <h2>KONSULTACJE</h2>
-
-                  <p>
-                    konsultacja z rodzicem po zakończeniu treningu- do 30 minut
-                  </p>
-                </div>
-              </div>
-
-              <div className="metric">
-                <div className="metric-icon">
-                  <img
-                    className="award"
-                    src="img/aside/offer_3/award-alt.svg"
-                  />
-                </div>
-
-                <div className="metric-info">
-                  <h2>PODSUMOWANIE</h2>
-
-                  <p>
-                    dyplom ukończenia treningu i opis funkcjonowania dziecka
-                  </p>
-                </div>
-              </div>
-            </aside> */}
+                );
+              })}
+            </aside>
+            
           </section>
         </div>
 
-        <div className="page-o1-1">
+        <div className={styles.singleList}>
           <section>
-            <h1>Cele treningu</h1>
-            <ul>
-              <li>
-                Rozpoznawanie emocji swoich i innych w poszczególnych sytuacjach
-              </li>
-              <li>Zrozumienie pojęcia „złość” i jej wyrażania</li>
-              <li>Uświadomienie istnienia granic między ludźmi</li>
-              <li>Nauka stawiania swoich granic i szanowanie granic innych</li>
-              <li>
-                Poznanie sposobów reagowania na zaczepki i agresję innych wobec
-                nas
-              </li>
-              <li>Poznanie znaczenia słowa „asertywność”</li>
-              <li>Rozróżnianie zachowań agresywnych, asertywnych i uległych</li>
-              <li>
-                Nauka rozpoznawania sytuacji, w której powinniśmy prosić o pomoc
-              </li>
-              <li>Zrozumienie pojęcia krytyka oraz informacja zwrotna</li>
-              <li>Kształtowanie umiejętności współpracy w grupie</li>
-              <li>Komplementowanie innych oraz reagowanie na komplementy</li>
-              <li>Rozpoczynanie rozmowy z innymi ludźmi</li>
-            </ul>
+            {offer3ListData.map(item => {
+              return(
+                <ListTemplate title={item.title} list={item.list}/>
+              )
+            })}
           </section>
         </div>
+
       </article>
     </>
   );

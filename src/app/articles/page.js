02ed38f7deaf735a5063e84245cb0b32496da734
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2024-10-20 16:49:18 +0200

    applying articles page with example unit with article preview

--- src/app/articles/page.js
+++ src/app/articles/page.js
@@ -1,8 +1,11 @@
 import styles from './articles.module.css';
+//
+import ArticlePreview from '../components/articlePreview/articlePreview.js';
 
 export default function Articles() {
     return(
-        <>
-        </>
+        <div className={styles.container}>
+            <ArticlePreview/>
+        </div>
     )
 }
--- src/app/components/articlePreview/articlePreview.js
+++ src/app/components/articlePreview/articlePreview.js
@@ -1,7 +1,21 @@
 import styles from './articlePreview.module.css';
 
-export default function ArticlePreviev() {
-    return(
-        <div className={styles.container}></div>
+export default function ArticlePreview() {
+    return (
+        <div className={styles.container}>
+            <h2>Ćwiczenia motoryki małej a rozwój mowy dziecka</h2>
+            <p>Prawidłowy rozwój mowy dziecka często zależy od wielu czynników, zarówno środowiskowych jak i psychologicznych. Ośrodek mowy w mózgu położony jest bardzo blisko tego, który odpowiada za ruchy rąk.
+                Oznacza to, iż stymulowanie dłoni i praca nad motoryką małą może pozytywnie wpłynąć na rozwój mowy.
+
+                Propozycje ćwiczeń motoryki małej:
+                - Wlejcie do miski wodę, wrzućcie do niej np. figurki zwierząt. Maluchy mogą wyławiać je rękami, starsze dzieci niech pożyczą od mamy z kuchni szczypce! Możecie zrobić sobie małe zawody – kto więcej wyłowi, rodzic czy dziecko?
+                - Do kolejnej zabawy potrzebujecie sznurka i klamerek do rozwieszania prania. Może ubrania lalek potrzebują odświeżenia? Po szybkim praniu dzieci niech rozwieszą je i przypną klamerkami do sznurka! Korzystając ze słonecznej pogody – kilka chwil i e!</p>
+            <div className={styles.dataContainer}>
+                <div className={styles.info}>
+                    <h3>MAGDALENA ŻAK | 01.01.2023</h3>
+                </div>
+                <a href='#'>Czytaj więcej</a>
+            </div>
+        </div>
     )
 }

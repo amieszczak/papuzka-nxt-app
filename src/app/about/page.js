1bfe672f7b812a7dac6bf95a297f9e0d4597e00f
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2024-10-26 12:48:47 +0200

    specialistAboutPageUnit on page with wariant-based component

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,20 @@
 import styles from './about.module.css';
 //
 import LandingPageAboutComponent from '../componentSets/landingPage/LPaboutComponent/landingPageAboutComponent.js';
-
+//
+import SpecialistAboutPageUnit from '../components/specialistAboutPageUnit/specialistAboutPageUnit.js';
+//
+import { aboutData } from '@/app/data/aboutData/aboutData.js';
 
 export default function About() {
     return(
         <div className={styles.container}>
             <LandingPageAboutComponent/>
+            {aboutData.map(item => {
+                return(
+                    <SpecialistAboutPageUnit {...item} variant={item.id % 2 == 0 ? 'reversed' : null}/>
+                )
+            })}
         </div>
     )
 }
--- src/app/components/specialistAboutPageUnit/specialistAboutPageUnit.js
+++ src/app/components/specialistAboutPageUnit/specialistAboutPageUnit.js
@@ -1,7 +1,56 @@
+'use client';
 import styles from './specialistAbout.module.css';
+//
+import Image from 'next/image';
+//
+import classNames from 'classnames';
+//
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+//
+import { useEffect } from 'react';
 
-export default function SpecialistAboutPageUnit() {
+const regularClasses = {
+    containerClass: styles.containerRegular,
+    photoClass: styles.photoContainerRegular,
+    contentClass: styles.contentContainerRegular,
+    photoBlinderClass: styles.photoBlinderRegular,
+}
+
+const reversedClasses = {
+    containerClass: styles.containerReversed,
+    photoClass: styles.photoContainerReversed,
+    contentClass: styles.contentContainerReversed,
+    photoBlinderClass: styles.photoBlinderReversed,
+}
+
+
+export default function SpecialistAboutPageUnit({name, description, photoSrc, variant}) {
+
+    useEffect(() => {
+        Aos.init();
+    }, [])
+
+    let classes;
+
+    switch(variant) {
+        case 'reversed':
+            classes = reversedClasses;    
+        break;
+        default: 
+            classes = regularClasses;
+    }
+
     return(
-        <></>
+        <div className={classNames({[styles.container]: true, [classes.containerClass]:true})}>
+            <div className={classNames({[styles.content]: true, [classes.contentClass]:true})}>
+                <h2>{name}</h2>
+                <p>{description}</p>
+            </div>
+            <div className={classNames({[styles.photoContainer]: true, [classes.photoClass]:true})}>
+                <Image src={photoSrc} width={660} height={510}/>
+                <div className={classNames({[styles.photoBlinder]:true, [classes.photoBlinderClass]:true})}></div>
+            </div>
+        </div>
     )
 }

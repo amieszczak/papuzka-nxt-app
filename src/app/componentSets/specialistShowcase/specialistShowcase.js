731a7251a78a0d58e3f18f5e457a7bf50e037337
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2024-11-04 21:46:29 +0100

    definition of specialistShowcase element with logic and simplified single-component structure

--- src/app/componentSets/specialistShowcase/specialistShowcase.js
+++ src/app/componentSets/specialistShowcase/specialistShowcase.js
@@ -9,6 +9,8 @@ import "swiper/css/autoplay";
 import "swiper/css/pagination";
 //
 import Image from 'next/image';
+import Link from 'next/link';
+//
 import { useState } from 'react';
 import classNames from 'classnames';
 
@@ -16,22 +18,25 @@ export default function SpecialistShowcase() {
 
     const [activeIndex, setActiveIndex] = useState(1);
 
+    const selectedDescription = specialistShowcaseData.find(item => item.id === activeIndex);
+
     return (
         <article>
-            <div classNames={styles.container}>
-                <h2>SPRAWDŹ DOSTĘPNOŚĆ NASZYCH SPECIALISTÓW</h2>
-                <div classNames={styles.carouselContainer}>
+            <div className={styles.container}>
+                <h2 className={styles.containerHeading}>SPRAWDŹ DOSTĘPNOŚĆ NASZYCH SPECIALISTÓW</h2>
+                <div className={styles.carouselContainer}>
                     <Swiper
                         className={styles.swiper}
                         modules={[Autoplay, Pagination]}
                         loop={true}
-                        /* autoplay={{
+                        autoplay={{
                             delay: 5000,
                             disableOnInteraction: true,
-                        }} */
-                        slidesPerView={3}
+                        }}
+                        slidesPerView={2.75}
                         centeredSlides={true}
                         spaceBetween={40}
+                        slideToClickedSlide={true}
                         onActiveIndexChange={(swiper) => {
                         setActiveIndex(swiper.realIndex)
                         console.log(swiper.realIndex);
@@ -39,7 +44,7 @@ export default function SpecialistShowcase() {
                         >
                         {specialistShowcaseData.map(item => {
                             return (
-                                <SwiperSlide key={item.id} className={classNames({[styles.therapistPhotoContainer]:true, [styles.therapistPhotoContainerActive]: item.id === activeIndex})}>
+                                <SwiperSlide key={item.id} className={classNames({[styles.therapistPhotoContainer]:true, [styles.therapistPhotoContainerActive]: item.id === activeIndex})} onClick={() => setActiveIndex(item.id)}>
                                     <Image src={item.src} 
                                         alt={item.alt}
                                         width={500}
@@ -50,6 +55,15 @@ export default function SpecialistShowcase() {
                         })}
                     </Swiper>
                 </div>
+                {selectedDescription ? 
+                    <div className={styles.descriptionContainer}>
+                        <h2 className={styles.descriptionHeading}>{selectedDescription.name}</h2> 
+                        <p>{selectedDescription.description}</p> 
+                        <div className={styles.linkContainer}>
+                            <Link href={selectedDescription.href} className={styles.link}>umów się na bezpłatną wizytę</Link>
+                        </div>
+                    </div>
+                : null}
             </div>
         </article>
     )

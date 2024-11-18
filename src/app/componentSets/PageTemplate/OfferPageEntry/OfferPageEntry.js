05c2d2112e21f7e794abe0415514dc88ce82f300
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2024-11-18 20:03:04 +0100

    'offer-page-entry' responsive correction with global styling adjustments and code removal

--- src/app/componentSets/PageTemplate/OfferPageEntry/OfferPageEntry.js
+++ src/app/componentSets/PageTemplate/OfferPageEntry/OfferPageEntry.js
@@ -1,5 +1,7 @@
 import styles from "./offer-page-entry.module.css";
+import classes from '../../../page.module.css';
 import Image from "next/image";
+/* import classNames from "classnames"; */
 
 export default function OfferPageEntry({
   pageTitle,
@@ -10,8 +12,8 @@ export default function OfferPageEntry({
     <>
       <section className={styles.entryContainer}>  
         <div className={styles.entryDescription}> 
-          <h1>{pageTitle}</h1>
-          <p>{entryInformation}</p>
+          <h1 className={classes.headingSecondary}>{pageTitle}</h1>
+          <p className={classes.paragraphPrimary}>{entryInformation}</p>
         </div>
 
         <div className={styles.entryPhotoContainer}>  

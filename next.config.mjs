0d0435f8352980b59bcf477c1bd4cc2a2be8231b
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-01-03 22:32:53 +0100

    Getting rid of redundant rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,6 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-   /*  async rewrites() {
-        return [
-            {
-                source: '/o-nas',
-                destination: '/pages/o-nas',
-            },
-            {
-                source: '/kontakt',
-                destination: '/pages/kontakt',
-            },
-            {
-                source: '/artykuly',
-                destination: '/pages/artykuly',
-            },
-            {
-                source: '/konsultacja-logopedyczna',
-                destination: '/pages/oferta/konsultacja-logopedyczna',
-            },
-            {
-                source: '/terapia-logopedyczna',
-                destination: '/pages/oferta/terapia-logopedyczna',
-            },
-            {
-                source: '/trening-umiejetnosci-spolecznych',
-                destination: '/pages/oferta/trening-umiejetnosci-spolecznych',
-            },
-            {
-                source: '/terapia-reki',
-                destination: '/pages/oferta/terapia-reki',
-            },
-            {
-                source: '/wystawienie-pisemnej-opinii',
-                destination: '/pages/oferta/wystawienie-pisemnej-opinii',
-            },
-        ]
-    }, */
-};
+const nextConfig = {};
 
 
 

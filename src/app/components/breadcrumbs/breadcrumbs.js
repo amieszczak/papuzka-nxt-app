2b631962af2783d491e328979fa8d814c703a9cc
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-03-04 21:33:19 +0100

    breadcrumbs component based on url, redundant code removal, data taken from 'usePathname' hook

--- src/app/components/breadcrumbs/breadcrumbs.js
+++ src/app/components/breadcrumbs/breadcrumbs.js
@@ -1,5 +1,4 @@
 'use client'
-import { useParams } from "next/navigation";
 import Link from "next/link";
 import styles from './breadcrumbs.module.css';
 import classes from '../../page.module.css';
@@ -11,113 +10,26 @@ import { usePathname } from "next/navigation";
 export default function Breadcrumbs() {
 
     const pathname = usePathname();
-
-    const params = useParams();
     
-    const slug = params?.slug;
-
-    let titleFromSlug;
-
-    if(slug != undefined) {
-        titleFromSlug = slug.charAt(0).toUpperCase() + slug.split('-').join(' ').slice(1);
-    }
-
-    //////////
-    
-    const [location, setLocation] = useState(null);
-    const [offer, setOffer] = useState(null);
-
-    function titleFromOffer(){
-       return offer.charAt(0).toUpperCase() + offer.split('-').join(' ').slice(1);
-    }
+    const [linkChain, setLinkChain] = useState(null)
 
     useEffect(() => {
-         
-        if(window.location.href.includes('oferta')) {
-            setLocation('oferta');
-            if(pathname.includes('konsultacja-logopedyczna')){
-                setOffer('Konsultacja logopedyczna');
-            }
-            if(pathname.includes('terapia-logopedyczna')){
-                setOffer('Terapia logopedyczna');
-            }
-            if(pathname.includes('trening-umiejetnosci-spolecznych')) {
-                setOffer('Trening umiejętności społecznych');
-            }
-            if(pathname.includes('terapia-reki')) {
-                setOffer('Terapia ręki');
-            }
-            if(pathname.includes('wystawienie-pisemnej-opinii')) {
-                setOffer('Wystawienie pisemnej opinii');
-            }
-        } 
 
-        if(window.location.href.includes('o-nas')) {
-            setLocation('o-nas');
-        } 
-        
-        if(window.location.href.includes('kontakt')) {
-            setLocation('kontakt');
-        } 
-
-        if(window.location.href.includes('artykuly') && slug != undefined) {
-            setLocation('artykul');
-        } else if(window.location.href.includes('artykuly')) {
-            setLocation('artykuly');
-        } 
-
-        if(!window.location.href.includes('oferta') 
-            && !window.location.href.includes('o-nas')
-            && !window.location.href.includes('kontakt')
-            && !window.location.href.includes('artykuly')){
-                setLocation(null);
+            if(pathname == '/') {
+                setLinkChain(null);
+                return;
             }
-            
-    
 
-        
+            const chainData = [{name: 'Strona główna', link: '/'}];
+
+            pathname.slice(1).split('/').forEach(item => {
+                const displayedTitle = item.charAt(0).toUpperCase() + item.split('-').join(' ').slice(1)
+                chainData.push({name: displayedTitle, link: item})
+            })
+
+            setLinkChain(chainData);
+
     },[pathname])
-    //////////
-
-    let linkChain;
-
-    switch(location) {
-        case 'oferta':
-            linkChain = [
-                {name: 'Strona główna', link: '/'},
-                {name: 'Oferta', link: '#'},   
-                {name: titleFromOffer(), link: '#'}        
-            ]
-        break;
-        case 'o-nas':
-            linkChain = [
-                {name: 'Strona główna', link: '/'},
-                {name: 'O nas', link: '#'}                
-            ]
-        break;
-        case 'kontakt':
-            linkChain = [
-                {name: 'Strona główna', link: '/'},
-                {name: 'Kontakt', link: '#'}                
-            ]
-        break;
-        case 'artykuly':
-            linkChain = [
-                {name: 'Strona główna', link: '/'},
-                {name: 'Artykuły', link: '#'}
-            ]
-        break;
-        case 'artykul':
-            linkChain = [
-                {name: 'Strona główna', link: '/'},
-                {name: 'Artykuły', link: '/artykuly'},
-                {name: titleFromSlug, link: '/artykuly/' + slug }
-            ]           
-        break;
-        default:
-            linkChain = null;
-        break;
-    }    
 
 
     return(
